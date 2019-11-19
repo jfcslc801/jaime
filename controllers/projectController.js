ef68444d9f4d454053537a82edce396058020731
jfrancoc <jfcslc@outlook.com>
2019-11-19 00:30:22 -0700

    multer

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,7 +1,12 @@
 // const fs = require('fs');
+const multer = require('multer');
 const Project = require('./../models/projectModel');
 const factory = require('./handlerFactory');
 
+const upload = multer({ dest: 'public/img/projects' });
+
+// multer upload photo
+exports.photoUpload = upload.single('photo');
 // GET ALL TOURS
 exports.getAllProjects = factory.getAll(Project);
 // GET Project
