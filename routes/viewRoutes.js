33e943b3fe7f2315638d9a8101e44bed1434a22d
jfrancoc <jfcslc@outlook.com>
2019-11-19 00:36:20 -0700

    create projects views

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 router.get('/project', viewsController.getProject);
+router.get('/create-projects', viewsController.createProject);
 
 module.exports = router;
